09915c80ab48e5ef89a08389b18f5642b27032ce
Dayoung <dbsek21@naver.com>
2023-04-19 21:31:02 +0900

    feat: 배경삭제

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -51,7 +51,7 @@ const Header = () => {
               <div className="wave" />
             </div>
           )
-              : <div className="ocean" />
+              : <div />
           
         }
       </Container>
