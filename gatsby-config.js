7c1d99f386f438d765c1535b40095f1c71387ff5
dayoung <dbsek21@gmail.com>
2026-06-22 22:13:12 +0900

    DEPLOY

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-offline`,
+    //`gatsby-plugin-offline`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
